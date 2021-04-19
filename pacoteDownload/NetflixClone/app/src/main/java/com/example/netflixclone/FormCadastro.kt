84548fc34eace57499ba314d3e0dc783e58ba422
Lucas Lisboa Alves <lucaslisboalves@hotmail.com>
2021-04-19 15:37:36 -0300

    Tela_cadastro
    Construindo o layout da tela de cadastro.

--- pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormCadastro.kt
+++ pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormCadastro.kt
@@ -2,10 +2,22 @@ package com.example.netflixclone
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.netflixclone.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormCadastroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_cadastro)
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        val toolbar = binding.toolbarCadastro
+        toolbar.setBackgroundColor(getColor(R.color.white))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_netflix_official_logo))
+
+
     }
 }
