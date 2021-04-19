24a922d21096694e4baa38db8b102ecd87afa6e5
Lucas Lisboa Alves <lucaslisboalves@hotmail.com>
2021-04-19 13:14:13 -0300

    Tela de login 1 e 2
    Ajustes nas primeiras telas do app.

---
+++ pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormCadastro.kt
@@ -0,0 +1,11 @@
+package com.example.netflixclone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
--- pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormLogin.kt
+++ pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormLogin.kt
@@ -1,11 +1,25 @@
 package com.example.netflixclone
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.netflixclone.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        binding.txtTelaCadastro.setOnClickListener{
+            val intent = Intent( this, FormCadastro::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
