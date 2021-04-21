0ad9ecd516f9308889a9a3f5faaceb48ea1480ae
Lucas Lisboa Alves <lucaslisboalves@hotmail.com>
2021-04-21 17:22:02 -0300

    Firebase
    cadastrando e autenticando usuários no projeto.

--- pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormLogin.kt
+++ pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormLogin.kt
@@ -1,11 +1,63 @@
 package com.example.netflixclone
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.netflixclone.databinding.ActivityFormLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar!!.hide()
+
+        binding.txtTelaCadastro.setOnClickListener{
+            val intent = Intent( this, FormCadastro::class.java)
+            startActivity(intent)
+
+        }
+
+        binding.btEntrar.setOnClickListener {
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+            val mensagem_erro = binding.mensagemError
+
+            if (email.isEmpty() || senha.isEmpty()){
+                mensagem_erro.setText("Preencha todos os campos")
+            }else{
+                        AutenticarUsuario()
+
+            }
+        }
+
+    }
+
+    private fun AutenticarUsuario(){
+
+        val email = binding.editEmail.text.toString()
+        val senha = binding.editSenha.text.toString()
+        val mensagem_erro = binding.mensagemError
+
+       FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener {
+           if (it.isSuccessful){
+               Toast.makeText(this, "Login efetuado com sucesso", Toast.LENGTH_SHORT).show()
+                IrParaTelaDeFilmes()
+           }
+       }.addOnFailureListener {
+            mensagem_erro.setText("Erro ao logar usuário")
+       }
+
+    }
+    private fun IrParaTelaDeFilmes(){
+        val intent = Intent(this,ListaFilmesKotlin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
