1191e2d9862936279d97170e5ca62c44868d90a0
Lucas Lisboa Alves <lucaslisboalves@hotmail.com>
2021-04-23 11:08:35 -0300

    73-AutenticandoUsuárioLogado

--- pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormLogin.kt
+++ pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/FormLogin.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.netflixclone.databinding.ActivityFormLoginBinding
+import com.google.firebase.FirebaseNetworkException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 class FormLogin : AppCompatActivity() {
 
@@ -16,6 +18,7 @@ class FormLogin : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar!!.hide()
+        VerificarUsuarioLogado()
 
         binding.txtTelaCadastro.setOnClickListener{
             val intent = Intent( this, FormCadastro::class.java)
@@ -51,10 +54,29 @@ class FormLogin : AppCompatActivity() {
                 IrParaTelaDeFilmes()
            }
        }.addOnFailureListener {
-            mensagem_erro.setText("Erro ao logar usuário")
+
+           var erro = it
+
+           when{
+               erro is FirebaseAuthInvalidCredentialsException -> mensagem_erro.setText(" E-mail ou senha estão incorretos.")
+               erro is FirebaseNetworkException -> mensagem_erro.setText(" Sem conexão com a internet.")
+               else -> mensagem_erro.setText("Erro ao logar usuário.")
+
+           }
+
        }
 
     }
+
+    private fun VerificarUsuarioLogado(){
+        val usuarioLogado = FirebaseAuth.getInstance().currentUser
+
+        if (usuarioLogado != null){
+            IrParaTelaDeFilmes()
+        }
+    }
+
+
     private fun IrParaTelaDeFilmes(){
         val intent = Intent(this,ListaFilmesKotlin::class.java)
         startActivity(intent)
