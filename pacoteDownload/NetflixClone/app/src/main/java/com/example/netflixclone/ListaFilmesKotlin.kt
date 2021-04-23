65b66c2d002741d6ca22b01cfe5c6a918ec2d268
Lucas Lisboa Alves <lucaslisboalves@hotmail.com>
2021-04-23 11:30:03 -0300

    74-deslogandoUsuário-Fiebase

--- pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/ListaFilmesKotlin.kt
+++ pacoteDownload/NetflixClone/app/src/main/java/com/example/netflixclone/ListaFilmesKotlin.kt
@@ -1,11 +1,46 @@
 package com.example.netflixclone
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.example.netflixclone.databinding.ActivityListaFilmesKotlinBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ListaFilmesKotlin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListaFilmesKotlinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_filmes_kotlin)
+        binding = ActivityListaFilmesKotlinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.menu_principal,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.deslogar -> {
+                                   FirebaseAuth.getInstance().signOut()
+                            }
+                         }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun VoltarTelaLogin(){
+        val intent = Intent(this, FormLogin::class.java  )
+        startActivity(intent)
+        finish()
     }
 }
